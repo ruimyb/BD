a152dbad4fd0dbe3893c230955cb2b7c34f50091
Ayutaya Rattanatray <rattanaa@ensipc163.imag.fr>
2015-11-30 16:29:09 +0100

    choix e

--- tp.java
+++ tp.java
@@ -25,11 +25,11 @@ public class tp {
 	    System.out.println("(e) Calculer le nombre de cadres de couvain pour une ruche\n");
 	    String str1 = sc.nextLine();
 	    System.out.println(str1);
-	    while ( (str1 != "a") || 
-		    (str1 != "b") || 
-		    (str1 != "c") || 
-		    (str1 != "d") || 
-		    (str1 != "e")  ){
+	   /* while ( (str1.equals("a") ) || 
+		    (str1 .equals("b")) || 
+		    (str1 .equals("c")) || 
+		    (str1 .equals("d")) || 
+		    (str1 .equals("e")) ){
 	    	System.out.println("Veuillez saisir à nouveau votre requete: \n");
 		    System.out.println("(a) Assembler une ruche et y placer un essaim  \n");
 		    System.out.println("(b) Transfert d'un cadre de couvain\n");
@@ -37,35 +37,41 @@ public class tp {
 		    System.out.println("(d) Calculer le poids moyen des cadres de hausse en fonction de la race des abeilles\n");
 		    System.out.println("(e) Calculer le nombre de cadres de couvain pour une ruche\n");
 		    str1 = sc.nextLine();
-	    }
+	    }*/
 	    switch (str1) {
 	    case "a": System.out.println("Veuillez saisir sur quelle table on travaille (Materiel, Hausse, Cadre, Couvercle, Toit, Plancher, Ruche, Essaim, EstCorpsRuche)");
 		break;
 		case "b": System.out.println("Veuillez saisir sur quelle table on travaille (Materiel, Hausse, Cadre, Couvercle, Toit, Plancher, Ruche, Essaim, EstCorpsRuche)");
 		break;
 		case "c": 
-				System.out.println("Vous avez choisi de calculer le poids de la récolte totale");
-	    		String S = "SELECT Sum(PoidsCadre) FROM Cadre c, Hausse h, EstCorpsRuche ecr, ruche r WHERE (h.NumHausse = c.NumHausse) AND (c.contenu = 'miel') AND (ecr.Type = Supplementaire) AND (r.CodeRuche = ecr.CodeRuche) AND (h.NumHausse = ecr.NumHausse) ";
-	    		ResultSet rs = stmt.executeQuery(S);
+				System.out.println("Vous avez choisi de calculer le poids de la récolte totale \n");
+	    		String s_c = "SELECT Sum(PoidsCadre) FROM Cadre c, Hausse h, EstCorpsRuche ecr, ruche r WHERE (h.NumHausse = c.NumHausse) AND (c.contenu = 'miel') AND (ecr.Type = Supplementaire) AND (r.CodeRuche = ecr.CodeRuche) AND (h.NumHausse = ecr.NumHausse) ";
+	    		ResultSet rs_c = stmt.executeQuery(s_c);
 	    		//	while (rs.next()) {
-				System.out.println(rs.getString("Sum(PoidsCadre)")) ;
+				System.out.println(rs_c.getString("Sum(PoidsCadre)")) ;
 	    		//}
 		break;
 		case "d": 
-				System.out.println("Vous avez choisi de calculer le poids moyen des cadres de hausses en fonction de la race des abeilles");
-				System.out.println("Veuillez choisir la race de la reine");
-				String  choixClient = sc.nextLine();
-				String S = "SELECT AVG(PoidsCadre) FROM Cadre c, Ruche r, Essaim e, EstCorpsRuche ecr WHERE (e.Race = " + choixClient + ") AND (e.CodeRuche = r.CodeRuche) AND (h.NumHausse = c.NumHausse) AND (c.Contenu = 'miel') AND (r.CodeRuche = ecr.CodeRuche) and (h.NumHausse = ecr.NumHausse)";
-				ResultSet rs = stmt.executeQuery(S);
-				System.out.println(rs.getString("AVG(PoidsCadre)")) ;
+				System.out.println("Vous avez choisi de calculer le poids moyen des cadres de hausses en fonction de la race des abeilles \n");
+				System.out.println("Veuillez choisir la race de la reine\n");
+				String  choixClient_d = sc.nextLine();
+				String s_d = "SELECT AVG(PoidsCadre) FROM Cadre c, Ruche r, Essaim e, EstCorpsRuche ecr WHERE (e.Race = '" + choixClient_d + "'') AND (e.CodeRuche = r.CodeRuche) AND (h.NumHausse = c.NumHausse) AND (c.Contenu = 'miel') AND (r.CodeRuche = ecr.CodeRuche) and (h.NumHausse = ecr.NumHausse)";
+				ResultSet rs_d = stmt.executeQuery(s_d);
+				System.out.println(rs_d.getString("AVG(PoidsCadre)")) ;
 		break;
-		case "e": System.out.println("Veuillez saisir sur quelle table on travaille (Materiel, Hausse, Cadre, Couvercle, Toit, Plancher, Ruche, Essaim, EstCorpsRuche)");
+		case "e": 
+				System.out.println("Vous avez choisi de calculer le nombre de cadre de couvain pour une ruche donnée \n");
+				System.out.println("Veuillez choisir le numero de la ruche \n");
+				String  choixClient_e = sc.nextLine();
+				String s_e = "SELECT COUNT(NumCadre) FROM Cadre c, Ruche r, EstCorpsRuche ecr WHERE (r.CodeRuche = '" + choixClient_e + "') AND (c.Contenu = 'couvain') AND (r.CodeRuche = ecr.CodeRuche) AND (c.NumHausse = ecr.NumHausse)";
+				ResultSet rs_e = stmt.executeQuery(s_e);
+				System.out.println(rs_e.getString("AVG(PoidsCadre)")) ;
 		break;
 	    default: System.out.println("Mauvais choix, recommencez");
 		break;
 	    }
 
-	    //String str2 = sc.nextLine();
+	    String str2 = sc.nextLine();
 	    switch (str2) {
 	    case "Materiel": System.out.println("Veuillez saisir sur quel composant vous intéresse (NumMateriel ou MatiereMateriel)");
 		break;
